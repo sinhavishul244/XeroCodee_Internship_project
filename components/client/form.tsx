5e0646485ce1e00442fc5b36e1038c3e6be02e0b
Vishul_Ubuntu <sinha.vishul244@gmail.com>
2023-07-28 06:09:29 +0530

    connected first api with the frontend

--- components/client/form.tsx
+++ components/client/form.tsx
@@ -1,26 +1,71 @@
 'use client'
 
 
-import React, { FormEvent, useState } from 'react'
-import { ToastContainer, toast } from 'react-toastify';
+import React, { FormEvent, useEffect, useState } from 'react'
+import { ToastContainer, toast, Slide } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Form = ({ placeholder, btnAction }: { placeholder?: string, btnAction?: string }) => {
 
     const [email, setEmail] = useState("");
+    const [isValidEmail, setIsValidEmail] = useState(false);
 
-    const submitHandler = (e: FormEvent): void => {
-        e.preventDefault();
-        toast.error('🦄 Wow so easy!', {
+    const emailSubmitter = async () => {
+
+        const id = toast.loading("Please wait...", {
             position: "top-right",
-            autoClose: 5000,
+            autoClose: 3000,
             hideProgressBar: true,
             closeOnClick: true,
             pauseOnHover: true,
             draggable: true,
             progress: undefined,
             theme: "colored",
+            toastId: "customId"
         });
+
+        const res = await fetch("/api/user/new", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email })
+        });
+        const data = await res.json();
+
+        data.success ? toast.update(id, { render: `${data.message}`, type: "success", isLoading: false }) : toast.update(id, { render: `${data.message}`, type: "error", isLoading: false })
+
+
+    }
+
+
+    useEffect(
+        () => {
+            const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+            setIsValidEmail(emailRegex.test(email));
+        }, [email])
+
+
+    const submitHandler = (e: FormEvent): void => {
+        e.preventDefault();
+        if (isValidEmail) {
+            emailSubmitter();
+
+
+
+        } else {
+            toast.error('Enter proper email!', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+                toastId: "customId"
+            });
+        }
     }
 
     return (
@@ -36,6 +81,7 @@ const Form = ({ placeholder, btnAction }: { placeholder?: string, btnAction?: st
                 draggable
                 pauseOnHover
                 theme="colored"
+                transition={Slide}
             />
             <form action="" onSubmit={submitHandler} className='form'>
                 <input type="text" name="email" id="" placeholder={placeholder} value={email} onChange={(e): void => { setEmail(e.target.value) }} />
