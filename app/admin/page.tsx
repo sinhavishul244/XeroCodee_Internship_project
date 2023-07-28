c7aa2d8814b462646b26a30c83c1c7de9d9e743b
Vishul_Ubuntu <sinha.vishul244@gmail.com>
2023-07-28 10:03:03 +0530

    added admin page to see all users

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,12 @@
 import User from '@/components/server/User'
 import React from 'react'
 import "../../styles/admin.scss"
+import { useRouter } from 'next/router'
 
 
 async function getData() {
-    const res = await fetch(`${process.env.API_URI}/api/user/all`, { method: "GET" })
+
+    const res = await fetch(`${process.env.API_URI}/api/user/all`, { method: "GET", next: { revalidate: 0 } })
     if (!res.ok) {
         throw new Error('Failed to fetch data')
     }
